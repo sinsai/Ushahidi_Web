931c4309acfe1cf0b0a581f0cae348fa6d0fc0c6
MATSUU Takuto <matsuu@gentoo.org>
2011-04-16 01:06:10 +0900

    Added thanks page for mobile.

---
+++ plugins/mobile/views/mobile/reports_submit_thanks.php
@@ -0,0 +1,13 @@
+<div class="report_submit">
+	<h2 class="main_title"><?php echo Kohana::lang('ui_main.reports_title_thanks'); ?></h2>
+	<!-- green-box -->
+	<div class="green-box">
+		<h3><?php echo Kohana::lang('ui_main.reports_submitted');?></h3>
+
+		<div class="thanks_msg"><a href="<?php echo
+			url::site().'mobile/reports' ?>"><?php echo Kohana::lang('ui_main.reports_return');?></a><br /><br /><br />
+			<?php echo Kohana::lang('ui_main.feedback_reports');?><br /><br />
+			<a href="https://spreadsheets.google.com/viewform?formkey=dGlnajlENUtFOFZnWlN2XzlqbklickE6MQ" class="btn_gray" target="_blank"><?php echo Kohana::lang('ui_main.feedback'); ?></a>
+		</div>
+	</div>
+</div>
