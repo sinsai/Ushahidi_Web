c43eed605bbcf9f35c50964dc29e0e7e34c84e07
Nachi Ueno <nati.ueno@gmail.com>
2011-05-07 17:30:24 +0900

    fixed some design issue

--- application/views/admin/reports_edit.php
+++ application/views/admin/reports_edit.php
@@ -103,7 +103,7 @@
 								<?php print form::textarea('incident_description', $form['incident_description'], ' rows="12" cols="40"') ?>
 								<h4><?php echo Kohana::lang('ui_main.tag');?>
 								<span><?php echo Kohana::lang('ui_main.tag_example');?>.</span> </h4>
-								<?php print form::textarea('tag', $form['tag'], ' rows="4" cols="40"') ?>
+								<?php print form::input('tag', $form['tag']) ?>
 								<h4><?php echo Kohana::lang('ui_main.admin_tag');?> 
 								<span><?php echo Kohana::lang('ui_main.admin_tag_example');?>.</span></h4>
 								<?php print form::textarea('admin_tag', $form['admin_tag'], ' rows="4" cols="40"') ?>
