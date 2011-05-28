2c0f6722bdf80400ab9afdf278e5da98214accb2
hayakasa <hayakasa@gmail.com>
2011-05-29 05:46:07 +0900

    Add static page in mobile plugin

---
+++ plugins/mobile/views/mobile/page.php
@@ -0,0 +1,9 @@
+<div id="content">
+	<div class="content-bg">
+		<div class="big-block">
+			<h2><?php echo Kohana::lang('ui_main.'.$page_title) ?></h2>
+			<div class="page_text"><?php echo
+htmlspecialchars_decode($page_description);?></div>
+		</div>
+	</div>
+</div>
