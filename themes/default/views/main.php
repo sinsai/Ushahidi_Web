eb018005d4e0b026be06cf66fb852a1ab99e1270
ohesotori <ohesotori@gmail.com>
2011-06-29 01:45:18 +0900

    #714 add banner, 'osmj japan'.

--- themes/default/views/main.php
+++ themes/default/views/main.php
@@ -219,6 +219,7 @@
                     <a href="http://www.hack4.jp/" target="_blank" rel="nofollow"><img src="http://sites.google.com/site/hackforjapan/RelatedInfo/unity/234x60.png" style="width:234px;height:60px;" alt="Hack For Japan 「コードでつなぐ。想いと想い」"></a>
                     <a href="http://www.311er.jp"><img src="http://www.311er.jp/wp-content/uploads/2011/04/bnr_160b.gif" title="Rescue311" width="160" height="80" /></a>
                     <a href="http://www.tomoni.net/"><img src="<?php echo url::base(); ?>themes/default/images/banner_tomoni.gif" title="tomoni.net" width="234" height="60" /></a>
+                    <a href="http://www.osmf.jp/"><img src="<?php echo url::base(); ?>themes/default/images/banner_osmfj.gif" title="osmf japan" width="233" height="100" /></a>
                 </div>
                 <!--banner area-->
 		<!-- / right column -->
