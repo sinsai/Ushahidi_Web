682aeca0775f33bfcd0d7e5869f8c1f4450eac73
Hiroshi Miura <miurahr@osmf.jp>
2011-07-03 15:03:34 +0900

    use applications/config/kml.php to set default limit
    Signed-off-by: Hiroshi Miura <miurahr@osmf.jp>

--- application/config/kml.php
+++ application/config/kml.php
@@ -1,3 +1,14 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 $config['cdn_kml_url'] = "http://cdn.sinsai.info.cache.yimg.jp/ushahidi/media/uploads/latest.kmz";
 $config['kmlsite'] = "http://www.sinsai.info/ushahidi/";
+$config['default_limit'] = 1000;
+// for google maps
+	// max file size :     3MB
+	// max raw KML size : 10MB
+	// max network link : 10
+	// max items        : 1000 <-- apply
+	// max items in view: 80
+    // --  in sinsai.info stat
+    // 1000items  6.8MB in raw KML, 0.5MB in KMZ
+    // 1500items  9.8MB in raw KML, 1.3MB in KMZ
+
