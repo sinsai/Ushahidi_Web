b22d1e0addc9025704e8e979edfb622f68fd660d
Hiroshi Miura <miurahr@linux.com>
2011-10-23 19:54:59 +0900

    fix typo Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- themes/default/views/reports.php
+++ themes/default/views/reports.php
@@ -180,7 +180,7 @@ echo '<input type="radio" name="order" value="dist" checked>近隣順';
 										{
 											?>
 											<a class="r_category" href="<?php echo url::site(); ?>reports/?c=<?php echo $category->id; ?>"><span class="r_cat-box"><img src="<?php echo url::base().Kohana::config('upload.relative_directory')."/".$category->category_image_thumb; ?>" height="16" width="16" /></span> <span class="r_cat-desc"><?php 
-    if(isset($localized_categories[(string)$category->category_title]){
+    if(isset($localized_categories[(string)$category->category_title])){
         echo $localized_categories[(string)$category->category_title];
     } else {
         echo $category->category_title;
