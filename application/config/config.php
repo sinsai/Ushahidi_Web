d4bab9c13956f6b3f066800543c83ffe4b55052e
Hal Seki <hal@georepublic.co.jp>
2011-11-18 09:46:31 +0900

    changed X_FORWARDED_PROTO to X_FORWARDED_FOR for applying nginx

--- application/config/config.php
+++ application/config/config.php
@@ -11,7 +11,7 @@ $config['site_domain'] = '/';
  * specified, then the current protocol is used, or when possible, only an
  * absolute path (with no protocol/domain) is used.
  */
-if ( (isset($_SERVER["HTTP_X_FORWARDED_PROTO"]) &&  $_SERVER["HTTP_X_FORWARDED_PROTO"]== 'https') || $_SERVER["SERVER_PORT"] == 443 ) {
+if ( (isset($_SERVER["HTTP_X_FORWARDED_FOR"]) &&  $_SERVER["HTTP_X_FORWARDED_FOR"]== 'https') ) {
     $config['site_protocol'] = 'https';
 } else {
     $config['site_protocol'] = 'http';
